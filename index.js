b3e62ecc92bbc83670415818eaccc2070f38af19
rogeriopvl <rogeriopvl@gmail.com>
2011-11-25 00:06:48 +0000

    Removed the .jpg extension from the image text when used

--- index.js
+++ index.js
@@ -8,44 +8,44 @@ server = require('node-router').getServer()
 im     = require('imagemagick')
 
 function fetch(query,cb){
-  var google = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8&q=' + encodeURI(query)
-  req({uri:google}, function (e, resp, body) {
-    result = JSON.parse(body)['responseData']['results'][0]
+    var google = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8&q=' + encodeURI(query)
+        req({uri:google}, function (e, resp, body) {
+            result = JSON.parse(body)['responseData']['results'][0]
 
-    if(result)
-      cb(result['unescapedUrl'])
-    else
-      cb("https://img.skitch.com/20111124-bqnf4a22pqxabc6pw4912g2i8e.jpg")
-  });
+            if(result)
+            cb(result['unescapedUrl'])
+            else
+            cb("https://img.skitch.com/20111124-bqnf4a22pqxabc6pw4912g2i8e.jpg")
+        });
 }
 
 function download(match, output, addText){
-  fetch(match, function(file){
-    var uri  = url.parse(file)
-      , host = uri.hostname
-      , path = uri.pathname
+    fetch(match, function(file){
+        var uri  = url.parse(file)
+        , host = uri.hostname
+        , path = uri.pathname
 
-    if(uri.protocol == "https:")
-      var r = https
-    else
-      var r = http
+        if(uri.protocol == "https:")
+        var r = https
+        else
+        var r = http
 
-    request = r.get({host: host, path: path}, function(res){
-      res.setEncoding('binary')
-      var img = ''
+        request = r.get({host: host, path: path}, function(res){
+            res.setEncoding('binary')
+            var img = ''
 
-      res.on('data', function(chunk) {
-        img += chunk
-      })
+            res.on('data', function(chunk) {
+                img += chunk
+            })
 
-      res.on('end', function(){
-        fs.writeFile(output, img, 'binary', function (err) {
-          if (err) throw err
+        res.on('end', function(){
+            fs.writeFile(output, img, 'binary', function (err) {
+                if (err) throw err
+            })
+            addText()
         })
-        addText()
-      })
+        })
     })
-  })
 }
 
 server.get("/", function(request, response){
@@ -54,46 +54,49 @@ server.get("/", function(request, response){
         response.write(data, 'utf-8');
         response.end();
     }
-  );
+    );
 })
 
 server.get("/favicon.ico", function(request, response){
-  return ""
+    return ""
 })
 
 server.get(new RegExp("^/(.*)(?:.jpg)?$"), function(request, response, match) {
-  var msg   = ""
+    var msg   = ""
     , match = escape(match)
     , chars = match.length
 
-  if(chars < 7)
-    msg = '"' + match.toUpperCase() + ' LIKE A BOSS!"'
-  else
-    msg = '"' + match.toUpperCase() + '\n LIKE A BOSS!"'
+    var aux = match.split('.');
+    match = aux.length == 2 && aux[1] == 'jpg' ? aux[0] : match;
 
-  var output = "/tmp/likeaboss-" + Math.floor(Math.random(10000000)*10000000) + '.jpg'
-  download(match, output, function(){
-    var args = [
-      output,
-      '-strokewidth', '2',
-      '-stroke', 'black',
-      '-fill', 'white',
-      '-pointsize', '50',
-      '-gravity', 'center',
-      '-weight', '800',
-      '-resize', '500x',
-      '-draw', 'text 0,100 ' + unescape(msg),
-      output
-    ]
+    if(chars < 7)
+        msg = '"' + match.toUpperCase() + ' LIKE A BOSS!"'
+    else
+        msg = '"' + match.toUpperCase() + '\n LIKE A BOSS!"'
 
-    im.convert(args, function(){
-      fs.readFile(output, function (err, data) {
-        if (err) throw err;
-        response.writeHead(200, {'Content-Type': 'image/jpeg' })
-        response.end(data)
-      });
+    var output = "/tmp/likeaboss-" + Math.floor(Math.random(10000000)*10000000) + '.jpg'
+    download(match, output, function(){
+        var args = [
+        output,
+    '-strokewidth', '2',
+    '-stroke', 'black',
+    '-fill', 'white',
+    '-pointsize', '50',
+    '-gravity', 'center',
+    '-weight', '800',
+    '-resize', '500x',
+    '-draw', 'text 0,100 ' + unescape(msg),
+    output
+        ]
+
+        im.convert(args, function(){
+            fs.readFile(output, function (err, data) {
+                if (err) throw err;
+                response.writeHead(200, {'Content-Type': 'image/jpeg' })
+                response.end(data)
+            });
+        })
     })
-  })
 })
 
 server.listen(process.env.PORT || 8080, '0.0.0.0')
